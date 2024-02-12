8fd2ab235133147886d839400604935aac23bca0
Sayed Rafiul Islam <sayed80@student.sust.edu>
2024-02-13 00:15:25 +0600

    responsive

--- app/actions/get-total-revenue.ts
+++ app/actions/get-total-revenue.ts
@@ -2,17 +2,11 @@ import axios from "axios"
 
 interface Item {
     orderedItem : {
-        _id : string,
+        productId : string,
         name : string,
-        isFeatured : boolean,
-        isArchieved : boolean,
         price : number,
-        createdAt : string,
-        categoryId : { name : string  },
-        sizeId : { name : string  },
-        colorId : { value : string  }
     },
-    _id : string
+    quantity : Number
 }
 
 interface Order {
@@ -27,12 +21,11 @@ interface Order {
 
 export const getTotalRevenue = async ( storeId : string ) => {
     const {data} = await axios(`${process.env.NEXT_PUBLIC_API_URL}/${storeId}/orders`)
-    console.log(data)
     if (data.length > 0) {
         const paidOrders = data.filter((order : Order) => order.isPaid === true)
         const totalRevenue = paidOrders.reduce((total : any, order : any) => {
-            const orderTotal = order.orderedItems.reduce((orderSum : any,{orderedItem} : any) => {
-                return orderSum = parseFloat(orderedItem?.price)
+            const orderTotal = order.orderedItems.reduce((orderSum : any,{orderedItem,quantity} : any) => {
+                return orderSum = parseFloat(orderedItem?.price) * quantity
             },0)
             return total + orderTotal
         },0)
